aac99702567698ccae92d62f3975ab39882d52d5
isanmartin <isanmartin@keedio.com>
2018-08-17 20:41:42 +0200

    Modified global var angularOpenshiftCheckAndCreateProject

--- vars/angularDisplayInstalledDependencies.groovy
+++ vars/angularDisplayInstalledDependencies.groovy
@@ -58,8 +58,10 @@ def call(body) {
         try {
             echo "List global dependencies ${showGlobalInstalledDependenciesDepthFlags}"
             sh "npm -g list ${showGlobalInstalledDependenciesDepthFlags}"
-        } catch(err) {
+        } catch(exc) {
             echo 'ERROR. There is an error retrieving NPM global dependencies'
+            def exc_message = exc.message
+            echo "${exc_message}"
         }
 
     }
@@ -113,14 +115,6 @@ def call(body) {
 
         try {
             echo "List local dependencies ${showLocalInstalledDependenciesDepthFlags} ${showLocalInstalledDependenciesTypeFlags}"
-/*
-            def sout = new StringBuilder(), serr = new StringBuilder()
-            def proc = "npm list ${showLocalInstalledDependenciesDepthFlags} ${showLocalInstalledDependenciesTypeFlags}".execute()
-            proc.consumeProcessOutput(sout, serr)
-            proc.waitForOrKill(30000)
-            echo "out> ${sout}"
-            echo "err> ${serr}"
- */
             sh "npm list ${showLocalInstalledDependenciesDepthFlags} ${showLocalInstalledDependenciesTypeFlags}"
         } catch(exc) {
             echo 'ERROR. There is an error retrieving NPM local dependencies'
