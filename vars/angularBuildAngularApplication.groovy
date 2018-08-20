3f98fc546204a9658175e1a852cd1d282ec954ec
isanmartin <isanmartin@keedio.com>
2018-08-20 12:59:09 +0200

    Added angularBuildAngularApplication global variable

---
+++ vars/angularBuildAngularApplication.groovy
@@ -0,0 +1,47 @@
+#!/usr/bin/groovy
+
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+
+    echo "angularBuildAngularApplication global variable parameters\n" +
+            "---------------------------------------------------------\n" +
+            "config.useBuildProdFlags: ${config.useBuildProdFlags} \n" +
+            "config.theBuildProdDefaultFlags: ${config.theBuildProdDefaultFlags} \n" +
+            "config.theBuildProdFlags: ${config.theBuildProdFlags} \n" +
+            "config.theBuildSpecificOutputPath: ${config.theBuildSpecificOutputPath} \n" +
+            "config.theInstallGloballyAngularCli: ${config.theInstallGloballyAngularCli} \n"
+
+
+    echo "Building angular application"
+
+    /***********************************************************
+     ************* BUILD PRODUCTION PARAMETERS *****************
+     ***********************************************************/
+
+    Boolean useBuildProdFlags = false
+    def buildProdFlags = config.theBuildProdDefaultFlags
+    Boolean installGloballyAngularCli = config.theInstallGloballyAngularCli
+
+    if (config.useBuildProdFlags) {
+        useBuildProdFlags = config.useBuildProdFlags.toBoolean()
+    }
+
+    if (useBuildProdFlags) {
+        buildProdFlags = config.theBuildProdFlags
+    } else {
+        echo "Build prod parameters default: ${buildProdFlags}"
+    }
+
+    echo "useBuildProdFlags: ${useBuildProdFlags}"
+    echo "buildProdFlags: ${buildProdFlags}"
+
+    if (installGloballyAngularCli) {
+        sh "ng build --prod ${buildProdFlags}"
+    } else {
+        sh "${angularCliLocalPath}ng build --prod ${buildProdFlags}"
+    }
+}
