b37420d9ace17157c8031c28db852a637d6365ca
isanmartin <isanmartin@keedio.com>
2018-08-20 20:08:48 +0200

    Added angularConfigureNPMRepository global variable

---
+++ vars/angularConfigureNPMRepository.groovy
@@ -0,0 +1,22 @@
+#!/usr/bin/groovy
+
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+
+    echo "angularConfigureNPMRepository global variable parameters\n" +
+            "--------------------------------------------------------\n" +
+            "config.theAngularNPMRepositoryURL: ${config.theAngularNPMRepositoryURL} \n"
+
+
+    def angularNPMRepositoryURL = config.theAngularNPMRepositoryURL
+
+    echo 'Setting Artifactory NPM registry'
+    sh "npm config set registry ${angularNPMRepositoryURL} "
+
+    sh "npm config get registry"
+
+}
