11d8e5fcd242fc9b7ff947a3f602bcf5da8b56a5
isanmartin <isanmartin@keedio.com>
2018-08-22 11:35:28 +0200

    Modified angularOpenshiftEnvironmentVariables global variable

--- vars/angularOpenshiftEnvironmentVariables.groovy
+++ vars/angularOpenshiftEnvironmentVariables.groovy
@@ -11,36 +11,54 @@ def call(body) {
 
     def branchNameHY = config.branchHY
     def branchType = config.branch_type
-    def mapEnvironmentVariables = config.map_environment_variables
+
 
     echo "angularOpenshiftEnvironmentVariables global variable parameters\n" +
             "---------------------------------------------------------------\n" +
             "branchNameHY: ${branchNameHY} \n" +
             "branchType: ${branchType} \n" +
-            "mapEnvironmentVariables: \n" +
-    mapEnvironmentVariables.each { key, value ->
-        echo "Environment variable: ${key} = ${value}"
-    }
+            "config.theMapEnvironmentVariables: \n" +
+            config.theMapEnvironmentVariables.each { key, value ->
+                echo "Environment variable: ${key} = ${value}"
+            }
 
 
     def packageJSON = readJSON file: 'package.json'
     def project = utils.getProject(packageJSON.name)
     def projectName = utils.getProjectName(packageJSON.name, branchType, branchNameHY)
+    def mapEnvironmentVariables = [:]
 
     echo "project: ${project}"
     echo "projectName: ${projectName}"
 
+
+    if (config.theMapEnvironmentVariables) {
+        mapEnvironmentVariables = config.theMapEnvironmentVariables
+    }
+
+    echo "mapEnvironmentVariables:"
     mapEnvironmentVariables.each { key, value ->
-        try {
-            echo "Removing $key environment variable"
-            sh "oc env dc/${project} $key- -n ${projectName}"
-        } catch (err) {
-            echo "The $key environment variable on dc/${project} -n ${projectName} cannot be removed"
+        echo "Map environment variable: ${key} = ${value}"
+    }
+
+    int mapEnvironmentVariablesSize = mapEnvironmentVariables.size()
+
+    echo "mapEnvironmentVariables size: ${mapEnvironmentVariablesSize}"
+
+    if (mapEnvironmentVariablesSize > 0) {
+
+        mapEnvironmentVariables.each { key, value ->
+            try {
+                echo "Removing $key environment variable"
+                sh "oc env dc/${project} $key- -n ${projectName}"
+            } catch (err) {
+                echo "The $key environment variable on dc/${project} -n ${projectName} cannot be removed"
+            }
+
+            echo "Adding $key=$value environment variable"
+            sh "oc env dc/${project} $key=$value -n ${projectName}"
+
         }
-
-        echo "Adding $key=$value environment variable"
-        sh "oc env dc/${project} $key=$value -n ${projectName}"
-
     }
 
     utils = null
